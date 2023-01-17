69731a427781cebaa77bb1c91af450e55d4285cb
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2023-01-17 17:20:20 +0200

    Add tests/conftest.py: Minimal Pytest fixtures app, client

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+
+"""
+conftest.py
+
+Pytest fixtures for testing the Korp backend as a Flask app.
+"""
+
+
+import pytest
+
+from korp import create_app
+
+
+@pytest.fixture()
+def app():
+    """Create and configure a Korp app instance."""
+    app = create_app()
+    # https://flask.palletsprojects.com/en/2.2.x/config/#TESTING
+    app.config["TESTING"] = True
+    yield app
+
+
+@pytest.fixture()
+def client(app):
+    """Create and return a test client."""
+    return app.test_client()
