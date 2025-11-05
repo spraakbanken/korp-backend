ebf775f6f2a27bceff6919588786e84c83f36ae0
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2025-11-05 13:02:49 +0200

    test_info: TestCorpusInfo: Test values, not only existence
    tests/functional/test_info.py:
    - TestCorpusInfo: Test values in the /corpus_info response, not only the
      existence of certain keys.

--- tests/functional/test_info.py
+++ tests/functional/test_info.py
@@ -54,22 +54,35 @@ class TestCorpusInfo:
 
     """Tests for the /corpus_info endpoint"""
 
+    def _get_corpora_info_sum(self, data, key):
+        """Return sum of info item key for all corpora in data["corpora"]."""
+        return sum(int(corpusdata["info"][key])
+                   for corpusdata in data["corpora"].values())
+
     def test_corpus_info(self, corpus_info, corpora):
         """Test /corpus_info for all corpora."""
         data = corpus_info(corpora)
-        assert data["total_size"]
-        assert data["total_sentences"]
+        assert len(data["corpora"]) == len(corpora)
+        assert (set(data["corpora"].keys())
+                == set(corpus.upper() for corpus in corpora))
+        assert data["total_size"] == self._get_corpora_info_sum(data, "Size")
+        assert data["total_sentences"] == self._get_corpora_info_sum(
+            data, "Sentences")
 
     @pytest.mark.parametrize(
-        "corpus", [
-            "testcorpus",
+        "corpus,attrs_p,attrs_s,attrs_a", [
+            ("testcorpus",
+             ["word", "lemma"],
+             ["text", "text_id", "paragraph", "paragraph_id",
+              "sentence", "sentence_id"],
+             []),
         ])
-    def test_corpus_info_single_corpus(self, corpus, corpus_info_single):
+    def test_corpus_info_single_corpus(self, corpus, attrs_p, attrs_s, attrs_a,
+                                       corpus_info_single):
         """Test /corpus_info for a single corpus."""
         data = corpus_info_single(corpus)
         attrs = data["attrs"]
-        # TODO: Add more specific assertions and perhaps split this
-        # into multiple tests
         assert attrs
-        assert attrs["p"]
-        assert attrs["s"]
+        assert attrs["p"] == attrs_p
+        assert attrs["s"] == attrs_s
+        assert attrs["a"] == attrs_a
