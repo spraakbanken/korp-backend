dfef08af2a016c2120e1304842a582cd770b1c2d
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2025-11-05 11:11:22 +0200

    test_info: Extract fixtures; split and parametrize test
    tests/functional/test_info.py:
    - Extract fixtures corpus_info and corpus_info_single from
      TestCorpusInfo.test_corpus_info_single_corpus.
    - Parametrise TestCorpusInfo.test_corpus_info_single_corpus by corpus.
    - Extract TestCorpusInfo.test_corpus_info from
      test_corpus_info_single_corpus.

--- tests/functional/test_info.py
+++ tests/functional/test_info.py
@@ -6,7 +6,38 @@ Pytest tests for the Korp /info and /corpus_info endpoints.
 """
 
 
+import pytest
+
 from tests.testutils import get_response_json
+
+
+@pytest.fixture
+def corpus_info(client, corpora):
+    """Yield function returning /corpus_info response for list of corpora."""
+
+    def _corpus_info(corpuslist):
+        """Return /corpus_info response for the corpora in corpuslist."""
+        data = get_response_json(
+            client(), "/corpus_info",
+            query_string={
+                "cache": "false",
+                "corpus": ",".join(corpus.upper() for corpus in corpuslist),
+            })
+        # print(data)
+        return data
+
+    yield _corpus_info
+
+
+@pytest.fixture
+def corpus_info_single(corpus_info):
+    """Yield function returning /corpus_info response for a single corpus."""
+
+    def _corpus_info_single(corpus):
+        """Return /corpus_info response for corpus (corpus-specific part)."""
+        return corpus_info([corpus])["corpora"][corpus.upper()]
+
+    yield _corpus_info_single
 
 
 class TestInfo:
@@ -23,22 +54,22 @@ class TestCorpusInfo:
 
     """Tests for the /corpus_info endpoint"""
 
-    def test_corpus_info_single_corpus(self, client, corpora):
+    def test_corpus_info(self, corpus_info, corpora):
+        """Test /corpus_info for all corpora."""
+        data = corpus_info(corpora)
+        assert data["total_size"]
+        assert data["total_sentences"]
+
+    @pytest.mark.parametrize(
+        "corpus", [
+            "testcorpus",
+        ])
+    def test_corpus_info_single_corpus(self, corpus, corpus_info_single):
         """Test /corpus_info for a single corpus."""
-        corpus = corpora[0].upper()
-        data = get_response_json(
-            client(), "/corpus_info",
-            query_string={
-                "cache": "false",
-                "corpus": corpus,
-            })
-        # print(data)
-        corpus_data = data["corpora"][corpus]
-        attrs = corpus_data["attrs"]
+        data = corpus_info_single(corpus)
+        attrs = data["attrs"]
         # TODO: Add more specific assertions and perhaps split this
         # into multiple tests
         assert attrs
         assert attrs["p"]
         assert attrs["s"]
-        assert data["total_size"]
-        assert data["total_sentences"]
