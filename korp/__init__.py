f1e67b78d9b3d74685d8469be4f04b5118510485
Martin Hammarstedt <martin.hammarstedt@gu.se>
2026-02-04 11:35:37 +0100

    Bump version to 8.3.0

--- korp/__init__.py
+++ korp/__init__.py
@@ -11,7 +11,7 @@ from korp.db import mysql
 from korp.memcached import memcached
 
 # The version of this script
-__version__ = "8.2.5"
+__version__ = "8.3.0"
 
 
 def create_app(config_override=None):
